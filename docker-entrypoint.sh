f41f921f9ce61578553891db5835423b555447c2
Thomas Kräftner <thomas@kraftner.com>
2020-05-25 15:35:14 +0200

    make uploads writeable

---
+++ docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Call default entrypoint
+docker-entrypoint.sh apache2
+#Chown wp-content to apache user to make it writeable for e.g. languages
+chown www-data:www-data /var/www/html/wp-content
+apache2-foreground
